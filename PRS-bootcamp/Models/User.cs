06fa73c8a0ceba4687b7d1fe24dcd999e815c063
Eric S <ydrive65@yahoo.com>
2017-10-02 11:32:27 -0400

    Connected to SQL Server

---
+++ PRS-bootcamp/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PRS_bootcamp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string UserName { get; set; }
+
+        [StringLength(10)]
+        [Required]
+        public string Password { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(12)]
+        [Required]
+        public string Phone { get; set; }
+
+        [StringLength(75)]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public bool IsAdmin { get; set; }
+
+        [Required]
+        public bool IsReviewer { get; set; }
+    }
+}

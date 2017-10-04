9ba1ffc245d0b21bdb657d46ac88bf6c4c771524
Eric S <ydrive65@yahoo.com>
2017-10-04 10:24:04 -0400

    Removed generated Razor Views and Controller Actions

--- PRS-bootcamp/Controllers/PurchaseRequestLineItemsController.cs
+++ PRS-bootcamp/Controllers/PurchaseRequestLineItemsController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using PRS_bootcamp.Models;
 using PRS_bootcamp.Utilities;
@@ -14,7 +8,6 @@ namespace PRS_bootcamp.Controllers
     public class PurchaseRequestLineItemsController : Controller
     {
         private PRS_bootcampContext db = new PRS_bootcampContext();
-
         private const string bind = "Id,PurchaseRequestId,ProductId,Quantity";
 
         public ActionResult Add([Bind(Include = bind)] PurchaseRequestLineItem purchaseRequestLineItem)
@@ -64,119 +57,7 @@ namespace PRS_bootcamp.Controllers
 
             return Json(new Msg { Result = "Success", Message = $"{numChanges} record(s) removed." });
         }
-
-        #region MVC_Actions
-        // GET: PurchaseRequestLineItems
-        public ActionResult Index()
-        {
-            var purchaseRequestLineItems = db.PurchaseRequestLineItems.Include(p => p.Product).Include(p => p.PurchaseRequest);
-            return View(purchaseRequestLineItems.ToList());
-        }
-
-        // GET: PurchaseRequestLineItems/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PurchaseRequestLineItem purchaseRequestLineItem = db.PurchaseRequestLineItems.Find(id);
-            if (purchaseRequestLineItem == null)
-            {
-                return HttpNotFound();
-            }
-            return View(purchaseRequestLineItem);
-        }
-
-        // GET: PurchaseRequestLineItems/Create
-        public ActionResult Create()
-        {
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "VendorPartNumber");
-            ViewBag.PurchaseRequestId = new SelectList(db.PurchaseRequests, "Id", "Description");
-            return View();
-        }
-
-        // POST: PurchaseRequestLineItems/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,PurchaseRequestId,ProductId,Quantity")] PurchaseRequestLineItem purchaseRequestLineItem)
-        {
-            if (ModelState.IsValid)
-            {
-                db.PurchaseRequestLineItems.Add(purchaseRequestLineItem);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "VendorPartNumber", purchaseRequestLineItem.ProductId);
-            ViewBag.PurchaseRequestId = new SelectList(db.PurchaseRequests, "Id", "Description", purchaseRequestLineItem.PurchaseRequestId);
-            return View(purchaseRequestLineItem);
-        }
-
-        // GET: PurchaseRequestLineItems/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PurchaseRequestLineItem purchaseRequestLineItem = db.PurchaseRequestLineItems.Find(id);
-            if (purchaseRequestLineItem == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "VendorPartNumber", purchaseRequestLineItem.ProductId);
-            ViewBag.PurchaseRequestId = new SelectList(db.PurchaseRequests, "Id", "Description", purchaseRequestLineItem.PurchaseRequestId);
-            return View(purchaseRequestLineItem);
-        }
-
-        // POST: PurchaseRequestLineItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,PurchaseRequestId,ProductId,Quantity")] PurchaseRequestLineItem purchaseRequestLineItem)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(purchaseRequestLineItem).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ProductId = new SelectList(db.Products, "Id", "VendorPartNumber", purchaseRequestLineItem.ProductId);
-            ViewBag.PurchaseRequestId = new SelectList(db.PurchaseRequests, "Id", "Description", purchaseRequestLineItem.PurchaseRequestId);
-            return View(purchaseRequestLineItem);
-        }
-
-        // GET: PurchaseRequestLineItems/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PurchaseRequestLineItem purchaseRequestLineItem = db.PurchaseRequestLineItems.Find(id);
-            if (purchaseRequestLineItem == null)
-            {
-                return HttpNotFound();
-            }
-            return View(purchaseRequestLineItem);
-        }
-
-        // POST: PurchaseRequestLineItems/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            PurchaseRequestLineItem purchaseRequestLineItem = db.PurchaseRequestLineItems.Find(id);
-            db.PurchaseRequestLineItems.Remove(purchaseRequestLineItem);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-#endregion
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)

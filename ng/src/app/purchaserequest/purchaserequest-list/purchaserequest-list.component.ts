6bf6dfd75273ce0ec29db093267ad11c3d0b350a
Eric S <ydrive65@yahoo.com>
2017-10-13 12:07:34 -0400

    Added core functionality

--- ng/src/app/purchaserequest/purchaserequest-list/purchaserequest-list.component.ts
+++ ng/src/app/purchaserequest/purchaserequest-list/purchaserequest-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import 'rxjs/add/operator/toPromise';
+
+import { PurchaseRequest } from '../../models/purchaserequest';
+import { User } from '../../models/user';
+
+import { PurchaseRequestService } from '../../services/purchaserequest.service';
+
 @Component({
   selector: 'app-purchaserequest-list',
   templateUrl: './purchaserequest-list.component.html',
@@ -7,9 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaserequestListComponent implements OnInit {
 
-  constructor() { }
+  requests: PurchaseRequest[];
+
+  getRequests(): void
+  {
+    this.prService.list().then(resp => this.requests = resp);
+  }
+
+  constructor(private prService: PurchaseRequestService) { }
 
   ngOnInit() {
+    this.getRequests();
   }
 
 }

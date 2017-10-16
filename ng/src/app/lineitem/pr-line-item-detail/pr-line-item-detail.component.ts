5e5101260441f71f768b2683d9557b53e9044075
Eric S <ydrive65@yahoo.com>
2017-10-16 11:57:21 -0400

    Added detail page

--- ng/src/app/lineitem/pr-line-item-detail/pr-line-item-detail.component.ts
+++ ng/src/app/lineitem/pr-line-item-detail/pr-line-item-detail.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/switchMap';
+
+import { LineItem } from '../../models/lineitem';
+
+import { PrLineItemService } from '../../services/pr-line-item.service';
 
 @Component({
   selector: 'app-pr-line-item-detail',
@@ -7,9 +15,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrLineItemDetailComponent implements OnInit {
 
-  constructor() { }
+  lineitem: LineItem;
+
+  delete(): void
+  {
+    this.lineitemService.delete(this.lineitem.Id);
+  }
+
+  edit(): void
+  {
+    this.router.navigate(['/lineitems/edit/' + this.lineitem.Id]);
+  }
+
+  getItems(): void
+  {
+    this.route.paramMap.switchMap(
+      (params: ParamMap) => this.lineitemService.get(params.get('id')))
+      .subscribe(resp => this.lineitem = resp);
+  }
+
+  constructor(
+    private lineitemService: PrLineItemService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getItems();
   }
 
 }

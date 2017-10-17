44b91d8716727b3d6bddf1bdd9dadb5b2278c016
Eric S <ydrive65@yahoo.com>
2017-10-17 09:45:36 -0400

    added getbyuser(id) function to service & commented

--- ng/src/app/services/purchaserequest.service.ts
+++ ng/src/app/services/purchaserequest.service.ts
@@ -1,9 +1,12 @@
+// core angular imports
 import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
+// rxjs operators
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/switchMap';
 
+// models
 import { PurchaseRequest } from '../models/purchaserequest';
 
 const urlBase: string = "http://localhost:51910/PurchaseRequests/";
@@ -11,36 +14,49 @@ const urlBase: string = "http://localhost:51910/PurchaseRequests/";
 @Injectable()
 export class PurchaseRequestService {
 
+  // add a request to the db
   add(pr: PurchaseRequest): Promise<any>
   {
     let url = urlBase + "Add";
     return this.http.post(url, pr).toPromise().then(resp => resp.json() || {}).catch(this.handleError);
   }
 
+  // remove a request from the db
   delete(id: number): Promise<any>
   {
     let url = urlBase + "Remove/" + id;
     return this.http.get(url).toPromise().then(resp => resp.json() || {}).catch(this.handleError);
   }
 
+  // get a request from the db
   get(id: string): Promise<PurchaseRequest>
   {
     let url = urlBase + "Get/" + id;
     return this.http.get(url).toPromise().then(resp => resp.json() as PurchaseRequest).catch(this.handleError);
   }
 
+  // get all requests by a given user
+  getByUser(id: number): Promise<PurchaseRequest[]>
+  {
+    let url = urlBase + "GetByUser/" + id;
+    return this.http.get(url).toPromise().then(resp => resp.json() as PurchaseRequest[]).catch(this.handleError);
+  }
+
+  // get all the requests from the db
   list(): Promise<PurchaseRequest[]>
   {
     let url = urlBase + "List";
     return this.http.get(url).toPromise().then(resp => resp.json() as PurchaseRequest[]).catch(this.handleError);
   }
 
+  // patch a request to the db
   update(pr: PurchaseRequest): Promise<any>
   {
     let url = urlBase + "Update";
     return this.http.post(url, pr).toPromise().then(resp => resp.json() || {}).catch(this.handleError);
   }
 
+  // handle any errors... or don't... w/e
   handleError(error): Promise<any>
   {
     console.error("YOU BROKE IT!", error);

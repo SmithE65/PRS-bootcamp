a80f56c8dbd555ff6f1412c78c65ce0dfec498cc
Eric S <ydrive65@yahoo.com>
2017-10-18 14:09:24 -0400

    renamed lineitems for proper data binding

--- ng/src/app/models/cart.ts
+++ ng/src/app/models/cart.ts
@@ -4,11 +4,11 @@ import { PurchaseRequest } from './purchaserequest';
 export class Cart
 {
   request: PurchaseRequest;
-  lineItems: PurchaseRequestLineItem[];
+  lineitems: PurchaseRequestLineItem[];
 
   constructor(request: PurchaseRequest, lineItems: PurchaseRequestLineItem[])
   {
     this.request = request;
-    this.lineItems = lineItems;
+    this.lineitems = lineItems;
   }
 }

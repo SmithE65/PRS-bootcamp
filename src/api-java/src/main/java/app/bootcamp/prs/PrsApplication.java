b8c0dea34fc3b178bdb3e9f639adbdd8c1788172
Eric Smith <ydrive65@yahoo.com>
2024-08-25 12:43:12 -0400

    Unzipped starter from Spring Initializr.

---
+++ src/api-java/src/main/java/app/bootcamp/prs/PrsApplication.java
@@ -0,0 +1,13 @@
+package app.bootcamp.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsApplication.class, args);
+	}
+
+}
---
+++ src/api-java/src/test/java/app/bootcamp/prs/PrsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bootcamp.prs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

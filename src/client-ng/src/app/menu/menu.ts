2234e3640aa3ba5ef20856d36763992a1edb0b6c
Eric Smith <ydrive65@yahoo.com>
2025-10-26 11:02:57 -0400

    Deleting old ng app and starting anew

--- src/client-ng/src/app/menu/menu.ts
+++ src/client-ng/src/app/menu/menu.ts
@@ -1,11 +1,34 @@
-export class Menu
-{
-    display: string;
-    href: string;
-    tooltip: string;
+// core angular
+import { Component, OnInit } from '@angular/core';
 
-    constructor(display: string, href: string, tooltip: string)
-    {
-        this.display = display; this.href = href; this.tooltip = tooltip;
-    }
+// services
+import { SystemService } from '../services/system.service';
+import { ShoppingCartService } from '../services/shopping-cart.service';
+
+// models
+import { User } from '../models/user';
+
+// menu item model
+import { MenuItem } from './menu';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  name = "Menu Component";
+
+  // inject services
+  constructor(
+    public cartService: ShoppingCartService,
+    public systemService: SystemService
+  ) { }
+
+  ngOnInit() {
+    console.log("menu init");
+    this.systemService.load();
+  }
+
 }
